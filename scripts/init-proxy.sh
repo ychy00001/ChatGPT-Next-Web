d6c745a6100aa47f9b11230f6b0df49f5c22ec2f
ychy00001 <yangchunyu00001@163.com>
2023-05-09 12:00:56 +0800

    update proxy port

--- scripts/init-proxy.sh
+++ scripts/init-proxy.sh
@@ -2,5 +2,5 @@ dir="$(dirname "$0")"
 config=$dir/proxychains.conf
 host_ip=$(grep nameserver /etc/resolv.conf | sed 's/nameserver //')
 echo "proxying to $host_ip"
-cp $dir/proxychains.template.conf $config 
-sed -i "\$s/.*/http $host_ip 7890/" $config
+cp $dir/proxychains.template.conf $config
+sed -i "\$s/.*/http $host_ip 7236/" $config
