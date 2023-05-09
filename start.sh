93afb0e482f2a883d50f304ce9f6c461f929f237
ychy00001 <yangchunyu00001@163.com>
2023-05-09 14:46:32 +0800

    update

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@ PID=$(ps aux | grep yarn | grep -v grep | awk '{ print $2 }')
 kill -9 $PID
 PID=$(ps aux | grep next | grep -v grep | awk '{ print $2 }')
 kill -9 $PID
-
+PID=$(ps aux | grep node | grep -v grep | awk '{ print $2 }')
+kill -9 $PID
 nohup yarn start -p 7236 2>1 >>run_dev.out &
--- stop.sh
+++ stop.sh
@@ -3,3 +3,5 @@ PID=$(ps aux | grep yarn | grep -v grep | awk '{ print $2 }')
 kill -9 $PID
 PID=$(ps aux | grep next | grep -v grep | awk '{ print $2 }')
 kill -9 $PID
+PID=$(ps aux | grep node | grep -v grep | awk '{ print $2 }')
+kill -9 $PID
