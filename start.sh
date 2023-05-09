fcfc6c1b133769985603d5a996b5436496287f41
ychy00001 <yangchunyu00001@163.com>
2023-05-09 12:25:40 +0800

    update shell

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+PID=$(ps aux | grep yarn | grep -v grep | awk '{ print $2 }')
+kill -9 $PID
+PID=$(ps aux | grep next | grep -v grep | awk '{ print $2 }')
+kill -9 $PID
+
 nohup yarn start -p 7236 2>1 >>run_dev.out &
--- stop.sh
+++ stop.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 PID=$(ps aux | grep yarn | grep -v grep | awk '{ print $2 }')
 kill -9 $PID
+PID=$(ps aux | grep next | grep -v grep | awk '{ print $2 }')
+kill -9 $PID
